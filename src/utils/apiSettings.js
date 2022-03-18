a7bf5085844df1f7ff9fef78edb10aabf1846c65
Rebaz Saleh <rebaz88.software@gmail.com>
2022-03-18 13:05:27 +0300

    remove trackterra as default endpoint

--- src/utils/apiSettings.js
+++ src/utils/apiSettings.js
@@ -1,12 +1,17 @@
-const SITE_URL = process.env.REACT_APP_API_URL ?? 'https://api.trackterra.org';
-const API_BASE_URL = `${SITE_URL}/api/v1`;
-console.log(`Fetching data from  ${SITE_URL}`);
+const API_BASE_URL = process.env.REACT_APP_API_URL;
+const API_URL = `${API_BASE_URL}/api/v1`;
+console.log(`Fetching data from  ${API_BASE_URL}`);
+
+if (!API_BASE_URL) {
+  alert('Please set the api url by creating a .env file and setting REACT_APP_API_URL variable');
+}
+
 export const apiOptions = (options) => {
   const apiOptions = {
-    baseURL: API_BASE_URL
+    baseURL: API_URL
   };
   Object.assign(apiOptions, options);
   return apiOptions;
 };
 
-export const getCsvFileUrl = (address, csvFile) => `${API_BASE_URL}/csv/txs/${address}/${csvFile}`;
+export const getCsvFileUrl = (address, csvFile) => `${API_URL}/csv/txs/${address}/${csvFile}`;
