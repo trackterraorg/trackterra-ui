b9a703a78eb9c04ef24085d55dd435f354a2ee4c
Rebaz Saleh <rebaz88.software@gmail.com>
2022-04-11 10:26:15 +0300

    add support for zen ledger

--- src/data/taxapps.js
+++ src/data/taxapps.js
@@ -2,5 +2,6 @@ export const TAX_APPS = [
   { value: 'koinly', label: 'Koinly' },
   { value: 'cointracker', label: 'Coin tracker' },
   { value: 'coinledger', label: 'Coin ledger' },
-  { value: 'cryptotaxcalculator', label: 'Crypto tax calculator' }
+  { value: 'cryptotaxcalculator', label: 'Crypto tax calculator' },
+  { value: 'zenledger', label: 'Zen ledger' }
 ];
