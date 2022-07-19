7c6de1074487727f4d0be7bfd04f7aac57350783
Rebaz Saleh <rebaz88.software@gmail.com>
2022-07-19 18:56:45 +0300

    temp fix: remove supported protocols

--- src/layouts/dashboard/SidebarConfig.js
+++ src/layouts/dashboard/SidebarConfig.js
@@ -8,27 +8,34 @@ import refreshOutline from '@iconify/icons-eva/refresh-outline';
 
 const getIcon = (name) => <Icon icon={name} width={22} height={22} />;
 
-const sidebarConfig = (chain, address) => [
-  {
-    title: 'Dashboard',
-    path: `/account/${chain}/${address}/dashboard`,
-    icon: getIcon(pieChart2Fill)
-  },
-  {
-    title: 'Transactions',
-    path: `/account/${chain}/${address}/txs`,
-    icon: getIcon(creditCardOutline)
-  },
-  {
-    title: 'Supported protocols',
-    path: `/account/${chain}/${address}/protocols`,
-    icon: getIcon(fileDoneOutline)
-  },
-  {
-    title: 'Reparse',
-    path: `/account/${chain}/${address}/reparse`,
-    icon: getIcon(refreshOutline)
+const sidebarConfig = (chain, address) => {
+  const urls = [
+    {
+      title: 'Dashboard',
+      path: `/account/${chain}/${address}/dashboard`,
+      icon: getIcon(pieChart2Fill)
+    },
+    {
+      title: 'Transactions',
+      path: `/account/${chain}/${address}/txs`,
+      icon: getIcon(creditCardOutline)
+    },
+    {
+      title: 'Reparse',
+      path: `/account/${chain}/${address}/reparse`,
+      icon: getIcon(refreshOutline)
+    }
+  ];
+
+  if (chain.toLowerCase() === 'lunc') {
+    urls.push({
+      title: 'Supported protocols',
+      path: `/account/${chain}/${address}/protocols`,
+      icon: getIcon(fileDoneOutline)
+    });
   }
-];
+
+  return urls;
+};
 
 export default sidebarConfig;
